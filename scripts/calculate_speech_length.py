87d73a48d29e0000ae30d8ca15e418f21f2e8f73
Lisa Malins <lkmalins@ucdavis.edu>
2022-07-29 21:35:44 -0700

    Updated other two python scripts with Shash's suggestions of proper IO syntax.
    Thanks again Shash you the best

--- scripts/calculate_speech_length.py
+++ scripts/calculate_speech_length.py
@@ -4,19 +4,17 @@ blocks = defaultdict(int)
 totals = defaultdict(int)
 averages = defaultdict(int)
 
-blocksource = open(snakemake.input[0], 'r')
-for line in blocksource.readlines():
-    blocks[line.split("\t")[0]] = line.split("\t")[1].rstrip('\n')
-blocksource.close()
+with open(snakemake.input[0], 'r') as blocksource:
+    for line in blocksource.readlines():
+        blocks[line.split("\t")[0]] = line.split("\t")[1].rstrip('\n')
 
-totalsource = open(snakemake.input[1], 'r')
-for line in totalsource.readlines():
-    totals[line.split("\t")[0]] = line.split("\t")[1].rstrip('\n')
-totalsource.close()
+with open(snakemake.input[1], 'r') as totalsource:
+    for line in totalsource.readlines():
+        totals[line.split("\t")[0]] = line.split("\t")[1].rstrip('\n')
 
 for entry in blocks.keys():
     averages[entry] = float(totals[entry]) / float(blocks[entry])
 
-output = open(snakemake.output[0], 'w')
-for c, l in averages.items():
-    output.write(c + "\t" + str(l) + '\n')
+with open(snakemake.output[0], 'w') as output:
+    for c, l in averages.items():
+        output.write(c + "\t" + str(l) + '\n')
